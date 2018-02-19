d2c87249412fc248eaa10161247c5847366626b8
ssheaks <sarah.sheaks@gmail.com>
2018-02-19 13:04:00 -0800

    finish set 1 drills

--- app.js
+++ app.js
@@ -26,12 +26,21 @@ function yearOfBirth(age) {
 
 
 function whoAmI(name, age) {
+  try {
+    if(typeof age === 1 || typeof name === 'string') {
+      throw new Error('Arguments not valid');
+    }
+  }
+  catch(e) {
+    console.log(e);
+  }
   let yob = yearOfBirth(age);
   console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-  console.log(`I was born in ${yob}`)
+  console.log(`I was born in ${yob}`);
+ 
 }
 
-whoAmI('Sarah', 33);
-yearOfBirth(24);
+whoAmI('Sarah');
+//yearOfBirth(24);
 
 
